2e60ace73bd15007ab8f391850c36ba03c638baf
Nathan Fallet <contact@nathanfallet.me>
2020-10-03 14:55:09 +0200

    Update des citations

--- src/quotes.py
+++ src/quotes.py
@@ -7,17 +7,19 @@ class Quote:
 # Définition des citations
 quotes = [
     # Citations de VVK
-    Quote("C'est moins simple", "Vincent Van Kerckhove"),
-    Quote("C'est moins marrant", "Vincent Van Kerckhove"),
-    Quote("On va pas être copain", "Vincent Van Kerckhove"),
+    Quote("C'est moins simple", "Vincent V.K."),
+    Quote("C'est moins marrant", "Vincent V.K."),
+    Quote("On va pas être copain", "Vincent V.K."),
+    Quote("C'est presque évident", "Vincent V.K."),
 
     # Citations de Stefano
-    Quote("Le cahier de prépa est à jour", "Stefano Sanguinetti"),
-    Quote("Reviens dans une heure", "Stefano Sanguinetti"),
+    Quote("Le cahier de prépa est à jour", "Stefano S."),
+    Quote("Reviens dans une heure", "Stefano S."),
+    Quote("Ne seront lues que les réponses dont les _résultats ou mots importants sont ENCADRÉS_, et dont on a vérifié l'_HOMOGÉNÉIRÉ_", "Stefano S."),
 
     # Citations de Baptiste
-    Quote("Tout seul on va plus vite, ensemble on va plus loin", "Baptiste Huber (proverbe africain)"),
+    Quote("Tout seul on va plus vite, ensemble on va plus loin", "Baptiste H. (proverbe africain)"),
 
     # Citations de René
-    Quote("M. RINGOT, DEVANT !", "René Lapole"),
+    Quote("M. RINGOT, DEVANT !", "René L."),
 ]
