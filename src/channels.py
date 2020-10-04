457eb7c29c57886208bdbe46a73d390f92f138e7
H2WO4 <arthurlamidel@gmail.com>
2020-10-04 13:45:10 +0200

    Restructuration compléte
    Les commandes passent toutes d'abord par l'event on_message, avant d'être redirigées vers leurs fonctions respective

--- src/channels.py
+++ src/channels.py
@@ -1,5 +1,3 @@
 """ Définition des listes choisissant les channels qui peuvent reçevoir des commandes et résponses """
 
-channelsForCommands = [762013664076627989, 617000315945746462]
-
 channelsForResponses = [762013664076627989, 617000315945746462]
--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from discord import Embed, Game
 
 # On importe nos ressources
 from quotes import random_quote
-from channels import channelsForCommands, channelsForResponses
+from channels import channelsForResponses
 
 
 
@@ -35,32 +35,17 @@ async def on_ready():
 """ Définition des commandes """
 
 # Commande de ping
-@bot.command()
 async def ping(ctx):
-    # Ne répond pas autre part que dans #bots
-    if ctx.channel.id not in channelsForCommands:
-        return
-    
     # On répond pong
-    await ctx.send('Pong')
+    await ctx.channel.send('Pong')
 
 # Guide de la contribution
-@bot.command()
 async def contribution(ctx):
-    # Ne répond pas autre part que dans #bots
-    if ctx.channel.id not in channelsForCommands:
-        return
-    
     # On explique comment fonctionne la contribution
-    await ctx.send("Pour contribuer au fonctionnement du bot et l'améliorer, rendez vous sur https://github.com/NathanFallet/MPSI.py")
+    await ctx.channel.send("Pour contribuer au fonctionnement du bot et l'améliorer, rendez vous sur https://github.com/NathanFallet/MPSI.py")
 
 # Commande de citation
-@bot.command()
 async def citation(ctx):
-    # Ne répond pas autre part que dans #bots
-    if ctx.channel.id not in channelsForCommands:
-        return
-    
     # On choisi une citation
     quote = random_quote()
 
@@ -69,7 +54,7 @@ async def citation(ctx):
     embed.set_footer(text=quote.author)
 
     # On envoit
-    await ctx.send(embed=embed)
+    await ctx.channel.send(embed=embed)
 
 
 
@@ -77,19 +62,28 @@ async def citation(ctx):
 
 # S'execute à chaque messages envoyé
 @bot.event
-async def on_message(ctx):
+async def on_message(message):
     # Ne répond pas à ses messages
-    if ctx.author == bot.user:
+    if message.author == bot.user:
         return
 
+    # Gestions des commandes
+    if message.content[0] == ".":
+        if message.content == ".ping":
+            await ping(message)
+        elif message.content == ".contribution":
+            await contribution(message)
+        elif message.content == ".citation":
+            await citation(message)
+
     # Ne répond pas autre part que dans #bots
-    if ctx.channel.id not in channelsForResponses:
+    if message.channel.id not in channelsForResponses:
         return
 
     # Transformation en DadBot
-    if "je suis " in ctx.content.lower():
-        response = ctx.content.lower().split("je suis ")[1]
-        await ctx.channel.send("Salut, " + response + ", je suis un robot !")
+    if "je suis " in message.content.lower():
+        response = message.content.lower().split("je suis ")[1]
+        await message.channel.send("Salut, " + response + ", je suis un robot !")
 
 
 
