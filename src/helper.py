f841f19507f37228b4787fc262a31e46cb208f18
Nathan Fallet <contact@nathanfallet.me>
2021-01-30 11:15:29 +0100

    Citations de la semaine 15 + quelques fix

--- src/helper.py
+++ src/helper.py
@@ -4,11 +4,19 @@ def isAlmostEqual(str1, str2):
     def convert(string):
         return list(filter(None, string.lower()
             .replace(".", "") # Les points
+            .replace("-", " ") # Les tirêts
             .replace("'", " ") # Les apostrophes
             .replace("’", " ")
             .replace("*", " ") # Le gras
             .replace("_", " ") # Le souligné
             .replace("!", "") # Les points d'exclamation
+            .replace("é", "e") # Les accents
+            .replace("è", "e")
+            .replace("ê", "e")
+            .replace("à", "a")
+            .replace("â", "a")
+            .replace("ô", "o")
+            .replace("î", "i")
         .split()))
 
     # On compare
